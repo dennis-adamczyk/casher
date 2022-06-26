c8a0cebd54370e04368166377e30e975238c2693
Dennis Adamczyk <ich@dennis-adamczyk.de>
2022-06-26 15:56:18 +0200

    Try fixing font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,11 +6,10 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel="preconnect" as="font" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" as="font" href="https://fonts.gstatic.com" crossOrigin="" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link
-            as="font"
-            href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" // should be display=optional
+            href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
             rel="stylesheet"
           />
         </Head>
