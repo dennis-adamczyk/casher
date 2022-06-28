755396219842e8f09dee12bdedebf7ff7549c32c
Tillwege <tillwege@gmail.com>
2022-06-28 23:23:52 +0200

    update the /api/cards route to use prisma as an example

--- pages/api/cards.ts
+++ pages/api/cards.ts
@@ -3,28 +3,11 @@ import { getAccounts, getSubscriptionsForBankAccount } from '@/data/database';
 import { apiError } from '@/helpers/api-error-handler';
 import type { NextApiRequest, NextApiResponse } from 'next';
 const fs = require('fs').promises;
+import { Bank_Account, PrismaClient } from '@prisma/client'
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse<BankCardData[]>) {
-  if (process.env.USE_SQL === 'true') {
-    try {
-      let cardRows = await getAccounts()
-      const Accounts = cardRows.map((row) => {
-        let account = {} as BankCardData;
-        account.id = row.id;
-        account.bank = row.bank;
-        account.bankName = row.bank_name;
-        account.holder = row.holder;
-        account.iban = row.iban;
-        account.balance = row.balance;
-        account.effectiveBalance = row.balance;
-        return account;
-      });
-      res.status(200).json(Accounts);
-    } catch (error) {
-      apiError(error, res)
-    }
-  } else {
-    const file = await fs.readFile('./data/cards.json');
-    res.status(200).json(JSON.parse(file));
-  }
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Bank_Account[]>) {
+  
+  const prisma = new PrismaClient()  
+  let accounts = await prisma.bank_Account.findMany()
+  res.status(200).json(accounts)
 }
