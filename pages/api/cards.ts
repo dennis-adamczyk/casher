9b827211a8c057db5355f0caeddcc4e0741c9304
Tillwege <tillwege@gmail.com>
2022-06-29 17:51:55 +0200

    migrate API routes to Prisma

--- pages/api/cards.ts
+++ pages/api/cards.ts
@@ -1,13 +1,13 @@
-import { BankCardData } from '@/components/common/BankCard';
-import { getAccounts, getSubscriptionsForBankAccount } from '@/data/database';
+import { DBClient } from '@/data/database';
 import { apiError } from '@/helpers/api-error-handler';
+import { Bank_Account } from '@prisma/client';
 import type { NextApiRequest, NextApiResponse } from 'next';
-const fs = require('fs').promises;
-import { Bank_Account, PrismaClient } from '@prisma/client'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<Bank_Account[]>) {
-  
-  const prisma = new PrismaClient()  
-  let accounts = await prisma.bank_Account.findMany()
-  res.status(200).json(accounts)
+  try {
+    let accounts = await DBClient.bank_Account.findMany()
+    res.status(200).json(accounts)
+  } catch (error) {
+    apiError(error, res)
+  }
 }
--- pages/api/subscriptions.ts
+++ pages/api/subscriptions.ts
@@ -1,32 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { getSubscriptions } from '@/data/database';
+import { DBClient, } from '@/data/database';
 import { apiError } from '@/helpers/api-error-handler';
+import { Subscription } from '@prisma/client';
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { SubscriptionData } from '../subscriptions';
-const fs = require('fs').promises;
-
-export default async function handler(req: NextApiRequest, res: NextApiResponse<SubscriptionData[]>) {
-  if (process.env.USE_SQL === 'true') {
-    try {
-      let rows = await getSubscriptions();
-      
-      const Subscriptions = rows.map((row) => {
-        let subscription = {} as SubscriptionData;
-        subscription.id = row.id;
-        subscription.name = row.name;
-        subscription.amount = row.amount;
-        subscription.categoryId = row.category_id;
-        subscription.bankAccountId = row.bank_account_id;
-        subscription.interval = row.interval;
-        return subscription;
-      });
-      res.status(200).json(Subscriptions);
-    } catch (error) {
-      apiError(error, res)
-    }
-    
-  } else {
-    const file = await fs.readFile('./data/subscriptions.json');
-    res.status(200).json(JSON.parse(file));
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Subscription[]>) {
+  try {
+    let subscriptions = await DBClient.subscription.findMany()
+    res.status(200).json(subscriptions)
+  } catch (error) {
+    apiError(error, res)
   }
 }
