e948790a540c1eead5f1e54486fef14ae63629b8
Tillwege <tillwege@gmail.com>
2022-06-30 22:03:22 +0200

    made goals have float values

---
+++ prisma/migrations/20220630200025_made_goals_work_with_flaots/migration.sql
@@ -0,0 +1,32 @@
+/*
+  Warnings:
+
+  - You are about to alter the column `amount` on the `Goal` table. The data in that column could be lost. The data in that column will be cast from `Int` to `Float`.
+  - You are about to alter the column `savings_amount` on the `Goal` table. The data in that column could be lost. The data in that column will be cast from `Int` to `Float`.
+  - You are about to alter the column `target_amount` on the `Goal` table. The data in that column could be lost. The data in that column will be cast from `Int` to `Float`.
+
+*/
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Goal" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "emojiIcon" TEXT,
+    "target_amount" REAL NOT NULL,
+    "amount" REAL NOT NULL,
+    "savings_amount" REAL NOT NULL,
+    "savings_interval" INTEGER NOT NULL,
+    "analysis_data_id" TEXT,
+    "bank_account_id" TEXT NOT NULL,
+    "history_id" TEXT,
+    CONSTRAINT "Goal_bank_account_id_fkey" FOREIGN KEY ("bank_account_id") REFERENCES "Bank_Account" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "Goal_analysis_data_id_fkey" FOREIGN KEY ("analysis_data_id") REFERENCES "Analysis_Data" ("id") ON DELETE SET NULL ON UPDATE CASCADE,
+    CONSTRAINT "Goal_history_id_fkey" FOREIGN KEY ("history_id") REFERENCES "Goal_History" ("id") ON DELETE SET NULL ON UPDATE CASCADE
+);
+INSERT INTO "new_Goal" ("amount", "analysis_data_id", "bank_account_id", "emojiIcon", "history_id", "id", "name", "savings_amount", "savings_interval", "target_amount") SELECT "amount", "analysis_data_id", "bank_account_id", "emojiIcon", "history_id", "id", "name", "savings_amount", "savings_interval", "target_amount" FROM "Goal";
+DROP TABLE "Goal";
+ALTER TABLE "new_Goal" RENAME TO "Goal";
+CREATE UNIQUE INDEX "Goal_analysis_data_id_key" ON "Goal"("analysis_data_id");
+CREATE UNIQUE INDEX "Goal_history_id_key" ON "Goal"("history_id");
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
